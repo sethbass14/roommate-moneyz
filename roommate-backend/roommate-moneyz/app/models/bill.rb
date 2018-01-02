c1697a4bf56e8d98b3149dd8d01749d574956375
c0nniewang <connie.wang303@gmail.com>
2018-01-02 16:00:05 -0500

    finished associations

--- roommate-backend/roommate-moneyz/app/models/bill.rb
+++ roommate-backend/roommate-moneyz/app/models/bill.rb
@@ -1,3 +1,5 @@
 class Bill < ApplicationRecord
   belongs_to :owner, :class_name => 'User'
+  has_many :payer_bills
+  has_many :payers, :class_name => 'User', through: :payer_bills
 end
--- roommate-backend/roommate-moneyz/app/models/house.rb
+++ roommate-backend/roommate-moneyz/app/models/house.rb
@@ -1,5 +1,7 @@
 class House < ApplicationRecord
   has_many :users
-  has_many :bills, through: :owners, :class_name => 'User'
+  has_many :owners, :class_name => 'User'
+  # has_many :payers, :class_name => 'User'
+  has_many :owned_bills, through: :owners, :foreign_key => :owner_id
 
 end
